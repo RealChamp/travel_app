881f72e7bc03f6be24bd3d0f4d561fab51c269bf
Matvey <matvey.w2015@mail.ru>
2020-09-30 14:37:56 +0400

    add destination screen

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -15,19 +15,19 @@ class Hotel {
 final List<Hotel> hotels = [
   Hotel(
     imageUrl: 'assets/images/hotel0.jpg',
-    name: 'Hotel 0',
+    name: 'Hotel 1',
     address: '404 Great St',
     price: 175,
   ),
   Hotel(
     imageUrl: 'assets/images/hotel1.jpg',
-    name: 'Hotel 1',
+    name: 'Hotel 2',
     address: '404 Great St',
     price: 300,
   ),
   Hotel(
     imageUrl: 'assets/images/hotel2.jpg',
-    name: 'Hotel 2',
+    name: 'Hotel 3',
     address: '404 Great St',
     price: 240,
   ),
